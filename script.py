4d50f68dceccb3357d89b20a031591e8522cfaa0
netphar <zagidull@lm2-304-26877.local>
2024-05-27 19:04:25 +0300

    code restructure re sample processing, additional dependence funcs added

--- script.py
+++ script.py
@@ -20,6 +20,50 @@ def calculate_entropy_faster(central_gene: str, local_network: list[str], df_inp
     ar_p = ar_corrs/np.sum(ar_corrs)
     entropy = -1.0*np.sum(ar_p * np.log2(ar_p))/ar_local.shape[0]
     return entropy
+    
+def dotproduct(x,y):
+    a=np.asarray(x)
+    b=np.asarray(y)
+    #return(np.dot(a,b)/(np.linalg.norm(a)*np.linalg.norm(b)))
+    return(np.dot(a,b))
+    
+def mi(x,y):
+	'''
+	https://www.biorxiv.org/content/10.1101/2024.05.09.593467v1.full.pdf
+	'''
+	a=np.asarray(x)
+	b=np.asarray(y)
+	r = pearsonr(a,b)[0]
+	return(-np.log((1-r**2))/2)
+    
+def calculate_entropy_alt(central_gene: str, local_network: list[str], df_input: pd.DataFrame, refcols: list[str], sample_colname: str='') -> float:
+    if not sample_colname:
+        cols_idx = df_input.columns.isin(refcols)
+    else:
+        cols = refcols + [sample_colname]
+        cols_idx = df_input.columns.isin(cols)
+    
+    ar_central = df_input.loc[central_gene,cols_idx].values
+    ar_local = df_input.loc[local_network,cols_idx].values
+    ar_corrs = np.apply_along_axis(func1d=pearsonr, axis=1, arr=ar_local, y=ar_central)
+    mi = -0.5*np.log2(1-ar_corrs[:,0]**2) # gaussian mutual information
+    ar_p = mi/np.sum(mi)
+    entropy = -1.0*np.sum(ar_p * np.log2(ar_p))/ar_local.shape[0]
+    return entropy
+
+def calculate_entropy_mi(central_gene: str, local_network: list[str], df_input: pd.DataFrame, refcols: list[str], sample_colname: str='') -> float:
+    if not sample_colname:
+        cols_idx = df_input.columns.isin(refcols)
+    else:
+        cols = refcols + [sample_colname]
+        cols_idx = df_input.columns.isin(cols)
+    
+    ar_central = df_input.loc[central_gene,cols_idx].values
+    ar_local = df_input.loc[local_network,cols_idx].values
+    ar_corrs = np.apply_along_axis(func1d=mi, axis=1, arr=ar_local, y=ar_central)
+    ar_p = ar_corrs/np.sum(ar_corrs)
+    ar_mi = np.sum(ar_p)/ar_local.shape[0]
+    return ar_mi
 
 def transform_df(df_input: pd.DataFrame, method: str) -> pd.DataFrame:
     if method=='log10':
@@ -67,10 +111,7 @@ def get_neighbours(genename_ensembl: str, df_ppi: pd.DataFrame) -> list[str]:
 def get_onehop(geneset: pd.DataFrame.index, df_ppi: pd.DataFrame) -> dict:
     return {k:get_neighbours(k, df_ppi) for k in geneset}
 
-def calculate_diffsd(central_gene: str, 
-                     df_input: pd.DataFrame, 
-                     refcols: list, 
-                     sample_colname: str) -> float:
+def calculate_diffsd(central_gene: str, df_input: pd.DataFrame, refcols: list, sample_colname: str) -> float:
     
     cols_idx = df_input.columns.isin(refcols)
     sd_ref = df_input.loc[central_gene, df_input.columns.isin(refcols)].std()
@@ -84,55 +125,26 @@ def calculate_diffsd(central_gene: str,
 
 if __name__ == "__main__":
 	os.chdir('/Users/zagidull/Documents/netzoo/')
-
-	# controls
-	# Tet-on control mice where BCR-ABL expression was suppressed N=3
-	cList1 = ['COHP_44959','COHP_44960','COHP_44961','COHP_44962','COHP_44963','COHP_44964','COHP_44965','COHP_44966','COHP_44967','COHP_44968',
-	         'COHP_44969','COHP_44970','COHP_44971','COHP_44972','COHP_44973','COHP_44974','COHP_44975','COHP_44976','COHP_44977']
-	cList2 = ['COHP_44978','COHP_44979','COHP_44980','COHP_44981','COHP_44982','COHP_44983','COHP_44984','COHP_44985','COHP_44986','COHP_44987',
-	         'COHP_44988','COHP_44989','COHP_44990','COHP_44991','COHP_44992','COHP_44993','COHP_44994','COHP_44995','COHP_44996']
-	cList3 = ['COHP_46947','COHP_46948','COHP_46949','COHP_46950','COHP_46951','COHP_46952','COHP_46953','COHP_46954','COHP_46955','COHP_46956',
-	         'COHP_46957','COHP_46958','COHP_46959','COHP_46960','COHP_46961','COHP_46962','COHP_46963','COHP_46964','COHP_46965']
-	# samples
-	# Tet-off CML mice had BCR-ABL expression that induced disease that mimics human chronic phase (CP) CML N=6 
-	sList1 = ['COHP_44940','COHP_44941','COHP_44942','COHP_44943','COHP_44944','COHP_44945','COHP_44946','COHP_44947','COHP_44948','COHP_44949','COHP_44950',
-	      'COHP_44951','COHP_44952','COHP_44953','COHP_44954','COHP_44955','COHP_44956','COHP_44957','COHP_44958']
-	sList2 = ['COHP_44997','COHP_44998','COHP_44999','COHP_45000','COHP_45001','COHP_45002','COHP_45003','COHP_45004','COHP_45005','COHP_45006','COHP_45007',
-	      'COHP_45008','COHP_45009','COHP_45010','COHP_45011','COHP_45012','COHP_45013','COHP_45014','COHP_45015']
-	sList3 = ['COHP_46927','COHP_46928','COHP_46929','COHP_46930','COHP_46931','COHP_46932','COHP_46933','COHP_46934','COHP_46935','COHP_46936','COHP_46937',
-	      'COHP_46938']
-	sList4 = ['COHP_46939','COHP_46940','COHP_46941','COHP_46942','COHP_46943','COHP_46944','COHP_46945','COHP_46946']
-	sList5 = ['COHP_49205','COHP_49206','COHP_49207','COHP_49208','COHP_49209','COHP_49210','COHP_49211','COHP_49212','COHP_49213','COHP_49214','COHP_49215',
-	      'COHP_49216','COHP_49217','COHP_49218','COHP_49219','COHP_49220','COHP_49221','COHP_49222','COHP_49223']
-	sList6 = ['COHP_49224','COHP_49225','COHP_49226','COHP_49227','COHP_49228','COHP_49229','COHP_49230','COHP_49231','COHP_49232','COHP_49233','COHP_49234',
-	      'COHP_49235','COHP_49236','COHP_49237','COHP_49238','COHP_49239','COHP_49240']
-
-	# reference for controls
-	df_c = pd.DataFrame([cList1, cList2, cList3])
-	df_c.columns = [f'wk{x}' for x in range(len(cList1))]
-	df_c.index = ['c1','c2','c3']
-	# reference for samples
-	df_s = pd.DataFrame([sList1, sList2, sList3, sList4, sList5, sList6])
-	df_s.columns = [f'wk{x}' for x in range(len(sList1))]
-	df_s.index = [f's{x}' for x in range(1,7,1)]
-	df_ref = pd.concat([df_c, df_s], axis=0, join='outer').replace({None: np.nan}) # samples as rows, timepoints as columns
-
-	# rnaseq data, it is qnormed i believe
-	df = pd.read_csv('GSE244990_cml_mrna_processed_1tpm_in_5_samples.tsv') # https://www.ncbi.xyz/geo/query/acc.cgi?acc=GSE244990
-	df.iloc[:,0] = df.iloc[:,0].str.split('.',expand=True).iloc[:,0].values
-	df.rename(columns={'Unnamed: 0':'GeneName'}, inplace=True)
-	df.set_index('GeneName', inplace=True)
-	df = df.loc[:, df_ref.stack().values] # leave only test and control samples
 	
-	# ppi
-	ppi = pd.read_csv('ppi_19k.csv')
+	# read
+	df_ref = pd.read_csv('raw/ref.csv', index_col=0)
+	df = pd.read_csv('raw/df.csv', index_col=0)
+	ppi = pd.read_csv('raw/ppi_19k.csv')
+	
+	# process
+	# select genes by var, ie filter out with var lower thant quantile percentage
+	# quantile = 0.8
+	# l_genevarC123 = cleanexp_byquant(transform_df(df.loc[:, df_ref.loc['c1':'c3',:].values.reshape(-1)], method='zeromean'), quantile=quantile)
+	# l_genevarS125 = cleanexp_byquant(transform_df(df.loc[:, df_ref.loc[['s1','s2','s5'],:].values.reshape(-1)], method='zeromean'), quantile=quantile)
+	# l_genevarS3 = cleanexp_byquant(transform_df(df.loc[:, df_ref.loc[['s3'],:].dropna(axis=1).values.reshape(-1)], method='zeromean'), quantile=quantile)
+	# l_genevarS4 = cleanexp_byquant(transform_df(df.loc[:, df_ref.loc[['s4'],:].dropna(axis=1).values.reshape(-1)], method='zeromean'), quantile=quantile)
+	# l_genevarS6 = cleanexp_byquant(transform_df(df.loc[:, df_ref.loc[['s6'],:].dropna(axis=1).values.reshape(-1)], method='zeromean'), quantile=quantile)
+	# geneset_var80perc = list(set(l_genevarC123 + l_genevarS125 + l_genevarS3 + l_genevarS4 + l_genevarS6))
+	# df_ppi_reducedbyvar = reduce_ppi(df_ppi_reduced, geneset_var80perc) 
 	ppi = reduce_ppi(ppi, df.index)
-	
-	#ppi + rnaseq
 	df = df.loc[filter_dfgenes_byppi(ppi, df.index), :]
 	holderDict = get_onehop(df.index, ppi)
 	
-		
 	holder = []
 	df_toprocess = transform_df(transform_df(df, method='ln'), method='zeromean')
 	d_neighbours = holderDict
@@ -147,11 +159,11 @@ if __name__ == "__main__":
 	    for sample in d_samples:
 	        
 	        params = [ [x, d_neighbours[x], df_week, d_controls ] for x in df_toprocess.index.tolist()]
-	        entropy_local = Parallel(n_jobs=n_jobs)(delayed(calculate_entropy_faster)(central_gene,local_network,df_input,refcols) \
+	        entropy_local = Parallel(n_jobs=n_jobs)(delayed(calculate_entropy_alt)(central_gene,local_network,df_input,refcols) \
 	                                           for central_gene,local_network,df_input,refcols in params)
 	        
 	        params = [ [x,d_neighbours[x],df_week,d_controls,sample] for x in df_toprocess.index.tolist()]
-	        entropy_sample = Parallel(n_jobs=n_jobs)(delayed(calculate_entropy_faster)(central_gene,local_network,df_input,refcols,sample_colname) \
+	        entropy_sample = Parallel(n_jobs=n_jobs)(delayed(calculate_entropy_alt)(central_gene,local_network,df_input,refcols,sample_colname) \
 	                                            for central_gene,local_network,df_input,refcols,sample_colname in params)
 	        ent_local = np.array(entropy_local)
 	        ent_local[np.isnan(ent_local)]=0
@@ -165,8 +177,10 @@ if __name__ == "__main__":
 	        sd_diff = np.array(sd_differential)
 	    
 	        entropy_differential = abs(ent_sample-ent_local)
+	        #temp.append(np.sum(entropy_differential)/len(df_toprocess.index))
 	        val=entropy_differential*sd_diff
 	        temp.append(np.sum(val)/len(df_toprocess.index))
+	    print(i, temp)
 	    holder.append(temp)	
 	
 	# combine data
@@ -175,15 +189,15 @@ if __name__ == "__main__":
 	df12to17= pd.DataFrame(holder[12:17], index=['wk12','wk13','wk14','wk15','wk16'], columns=['s1','s2','s5','s6']).T	
 	df17end= pd.DataFrame(holder[17:], index=['wk17','wk18'], columns=['s1','s2','s5']).T
 	df_wk0toend_individual = pd.concat([df0to8, df8to12, df12to17, df17end], axis=1, join="outer") # indi animals
-	df_wk0toend_individual.to_csv('wk0toend_ind_zeromeanln_8kppi.csv')
+	df_wk0toend_individual.to_csv('saves/wk0toend_ind_zeromeanln_8kppi_gaussMI.csv')
 	
 	# plot individual curves
 	fig, ax = plt.subplots(figsize=(18, 8))
 	plt.plot(df_wk0toend_individual.T.fillna(0), label=df_wk0toend_individual.T.columns)	
 	ax.set_xticks(range(len(df_wk0toend_individual.T.index)), labels=df_wk0toend_individual.T.index)
 	ax.set_ylabel('global differential entropy')
-	ax.set_title('global per-sample entropy on tf-ppi (7.8k pairs) using zeromean-ln rnaseq data. s4 died on wk7, s3 on 11 and s6 on wk16')
+	ax.set_title('global per-sample entropy on tf-ppi (8k pairs) using zeromean-ln rnaseq data and gaussMI. s4 died on wk7, s3 on 11 and s6 on wk16')
 	ax.legend()
-	plt.savefig('wk0toend_ind_zeromeanln_8kppi.png')
+	plt.savefig('saves/wk0toend_ind_zeromeanln_8kppi_gaussMI.png')
 
 
